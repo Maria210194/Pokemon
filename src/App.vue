a3f0a878492122b664a5192334bd1849fff93dfe
Maria210194 <mary.stachi@gmail.com>
2022-08-07 15:47:23 +0200

    urls for pokeapi

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,20 @@
   </div>
 </template>
 
+
+<!-- 
+    pokemonsURL: "https://pokeapi.co/api/v2/pokemon?limit="
+
+    pokemonImageURL: "https:://raw.githubcontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/",
+    
+    other possible image urls: 
+    "https:://raw.githubcontent.com/PokeAPI/sprites/master/sprites/pokemon/{ID}.png"
+    "https:://raw.githubcontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{ID}.png"
+    "https:://raw.githubcontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/{ID}.png"<template>
+    
+    pokemonURL: "https://pokeapi.com/api/v2/pokemon/"
+-->
+
 <script>
 import HeaderComponent from '@/components/HeaderComponent.vue';
 import PokemonList from './components/PokemonList.vue';
